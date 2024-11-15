7f4654c4158a497754e514e0fe19bb3af57a1a2e
BKDR01 <bahodirov.bobur@icloud.com>
2024-11-16 01:20:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,176 @@
+function set1() {
+    let set1pr = +prompt("Qiyqiriq сет dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 45000
+    let set1za = confirm(`siz ${set1pr} ta Qiyqiriq сет ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set2() {
+    let set1pr = +prompt("Снек сет dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 79000
+    let set1za = confirm(`siz ${set1pr} ta Снек сет ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set3() {
+    let set1pr = +prompt("Классик сет dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 34000
+    let set1za = confirm(`siz ${set1pr} ta Классик сет ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set4() {
+    let set1pr = +prompt("Лонгер рингс сет dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 45000
+    let set1za = confirm(`siz ${set1pr} ta Лонгер рингс сет ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+
+function set5() {
+    let set1pr = +prompt("BIG сет dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 62000
+    let set1za = confirm(`siz ${set1pr} ta BIG сет ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set6() {
+    let set1pr = +prompt("Лестер сет dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 53000
+    let set1za = confirm(`siz ${set1pr} ta Лестер сет ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set7() {
+    let set1pr = +prompt("Ckul сет dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 36000
+    let set1za = confirm(`siz ${set1pr} ta ckul сет ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set8() {
+    let set1pr = +prompt("Комбо сет Большой dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 20000
+    let set1za = confirm(`siz ${set1pr} ta Комбо сет Большой ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+}
+function set9() {
+    let set1pr = +prompt("Do’stlar 1х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 43000
+    let set1za = confirm(`siz ${set1pr} ta Do’stlar 1х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set10() {
+    let set1pr = +prompt("Do’stlar 2х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 79000
+    let set1za = confirm(`siz ${set1pr} ta Do’stlar 2х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set11() {
+    let set1pr = +prompt("Do’stlar 4х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 149000
+    let set1za = confirm(`siz ${set1pr} ta Do’stlar 4х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set11() {
+    let set1pr = +prompt("Do’stlar 4х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 149000
+    let set1za = confirm(`siz ${set1pr} ta Do’stlar 4х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set11() {
+    let set1pr = +prompt("Do’stlar 4х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 149000
+    let set1za = confirm(`siz ${set1pr} ta Do’stlar 4х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set11() {
+    let set1pr = +prompt("Do’stlar 4х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 149000
+    let set1za = confirm(`siz ${set1pr} ta Do’stlar 4х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set11() {
+    let set1pr = +prompt("Do’stlar 4х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 149000
+    let set1za = confirm(`siz ${set1pr} ta Do’stlar 4х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
+function set12() {
+    let set1pr = +prompt("Острый Do’stlar 1х dan nechta zakaz qilmoqchi siz ?")
+    let set1itog = set1pr * 43000
+    let set1za = confirm(`siz ${set1pr} ta Острый Do’stlar 1х ingizni narxi ${set1itog} bo'ladi \n \n Zakaz qilasizmi`)
+        if (set1za) {
+            alert("Zakaziz qabul qilindi lekin baribir kemidi kutmen 😂😂😂 \n \n Bu sizning ustingizdan  kulish emas prosta prikol !!!")
+        } else {
+            alert("Zakaziz qabul qilmadi")
+        }
+
+}
